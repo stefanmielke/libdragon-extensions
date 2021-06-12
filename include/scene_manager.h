ac5b3a152515c42c0e740a604fab6d8443001ef2
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-12 20:51:00 -0300

    Initial Scene Manager implementation.

---
+++ include/scene_manager.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <libdragon.h>
+#include "mem_pool.h"
+
+// Called by scene_manager_tick after fnSMSceneChangeCallback.
+typedef void (*fnSMCreateCallback)();
+// Called by scene_manager_tick. Should return next screen_id (same if didn't change)
+typedef short (*fnSMTickCallback)();
+// Called by scene_manager_display.
+typedef void (*fnSMDisplayCallback)(display_context_t disp);
+// Called by scene_manager_tick before fnSMSceneChangeCallback.
+typedef void (*fnSMDestroyCallback)();
+// Called by scene_manager_tick a frame after the user calls scene_manager_change_scene.
+// Should call scene_manager_set_callbacks to set up the scene.
+typedef bool (*fnSMSceneChangeCallback)(short from_scene_id, short to_scene_id);
+
+typedef struct {
+	fnSMCreateCallback create;
+	fnSMTickCallback tick;
+	fnSMDisplayCallback display;
+	fnSMDestroyCallback destroy;
+} SceneCallbacks;
+
+typedef struct {
+	short current_scene_id;
+	short next_scene_id;
+	MemZone *scene_memory_pool;
+	fnSMSceneChangeCallback change_scene_callback;
+	SceneCallbacks scene_callbacks;
+} SceneManager;
+
+// Initializes the Scene Manager
+// If scene_memory_pool is NULL, it will not free memory on screen changes.
+SceneManager *scene_manager_init(MemZone *memory_pool, MemZone *scene_memory_pool,
+								 fnSMSceneChangeCallback change_scene_callback);
+
+// Marks scene to change to the scene_id.
+void scene_manager_change_scene(SceneManager *scene_manager, short scene_id);
+
+// Sets all the callbacks for your scene. Should be called when changing scenes on your
+// fnSMSceneChangeCallback.
+// create_callback and destroy_callback are optional.
+void scene_manager_set_callbacks(SceneManager *scene_manager, fnSMCreateCallback create_callback,
+								 fnSMTickCallback tick_callback,
+								 fnSMDisplayCallback display_callback,
+								 fnSMDestroyCallback destroy_callback);
+
+// Ticks the scene manager. Will change the scene if needed.
+void scene_manager_tick(SceneManager *scene_manager);
+
+void scene_manager_display(SceneManager *scene_manager, display_context_t disp);
---
+++ src/scene_manager.c
@@ -0,0 +1,51 @@
+#include "../include/scene_manager.h"
+
+SceneManager *scene_manager_init(MemZone *memory_pool, MemZone *scene_memory_pool,
+								 fnSMSceneChangeCallback change_scene_callback) {
+	SceneManager *scene_manager = mem_zone_alloc(memory_pool, sizeof(SceneManager));
+	scene_manager->change_scene_callback = change_scene_callback;
+	scene_manager->current_scene_id = -1;
+	scene_manager->scene_memory_pool = scene_memory_pool;
+
+	return scene_manager;
+}
+
+void scene_manager_change_scene(SceneManager *scene_manager, short scene_id) {
+	scene_manager->next_scene_id = scene_id;
+}
+
+void scene_manager_set_callbacks(SceneManager *scene_manager, fnSMCreateCallback create_callback,
+								 fnSMTickCallback tick_callback,
+								 fnSMDisplayCallback display_callback,
+								 fnSMDestroyCallback destroy_callback) {
+	scene_manager->scene_callbacks.create = create_callback;
+	scene_manager->scene_callbacks.tick = tick_callback;
+	scene_manager->scene_callbacks.display = display_callback;
+	scene_manager->scene_callbacks.destroy = destroy_callback;
+}
+
+void scene_manager_tick(SceneManager *scene_manager) {
+	// change scene if needed
+	if (scene_manager->current_scene_id != scene_manager->next_scene_id) {
+		if (scene_manager->current_scene_id >= 0) {
+			if (scene_manager->scene_callbacks.destroy)
+				scene_manager->scene_callbacks.destroy();
+			if (scene_manager->scene_memory_pool)
+				mem_zone_free_all(scene_manager->scene_memory_pool);
+		}
+
+		scene_manager->change_scene_callback(scene_manager->current_scene_id,
+											 scene_manager->next_scene_id);
+
+		scene_manager->current_scene_id = scene_manager->next_scene_id;
+
+		if (scene_manager->scene_callbacks.create)
+			scene_manager->scene_callbacks.create();
+	}
+
+	scene_manager->next_scene_id = scene_manager->scene_callbacks.tick();
+}
+
+void scene_manager_display(SceneManager *scene_manager, display_context_t disp) {
+	scene_manager->scene_callbacks.display(disp);
+}
