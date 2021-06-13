0cee33b074bcdba7142b7841fdb42ae4b81b4714
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-12 23:51:45 -0300

    Document mem_pool.h

--- include/mem_pool.h
+++ include/mem_pool.h
@@ -3,18 +3,39 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-// A contiguous zone where memory can be allocated.
+/**
+ * @brief A contiguous zone where memory can be allocated.
+ */
 typedef struct {
-    char* pos;    // Pointer to current free space position.
-    char* start;  // Pointer to start of zone.
-    char* end;    // Pointer to end of zone.
+	/// Pointer to current free space position.
+	char *pos;
+	/// Pointer to start of zone.
+	char *start;
+	/// Pointer to end of zone.
+	char *end;
 } MemZone;
 
-// Allocate a memory zone with the given size.
-void mem_zone_init(MemZone *z, size_t size);
+/**
+ * @brief Allocate a memory zone with the given size.
+ *
+ * @param memory_zone MemZone to use.
+ * @param size Size in bytes that the MemZone will have. (eg.: '1*1024*1024' for 1MB)
+ */
+void mem_zone_init(MemZone *memory_zone, size_t size);
 
-// Allocate memory from the zone.
-void *mem_zone_alloc(MemZone *z, size_t size);
+/**
+ * @brief Allocate memory from the zone.
+ *
+ * @param memory_zone MemZone to use.
+ * @param size Size in bytes of the memory. (eg.: sizeof(int))
+ *
+ * @return the memory allocated.
+ */
+void *mem_zone_alloc(MemZone *memory_zone, size_t size);
 
-// Free all objects in the zone.
-void mem_zone_free_all(MemZone *z);
+/**
+ * @brief Free all objects in the zone.
+ *
+ * @param memory_zone MemZone to use.
+ */
+void mem_zone_free_all(MemZone *memory_zone);
