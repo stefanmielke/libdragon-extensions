51d3880a173772d3b9a90461f7d11c9ac7174db8
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-13 00:15:31 -0300

    Document spritesheet.h

--- include/spritesheet.h
+++ include/spritesheet.h
@@ -3,6 +3,16 @@
 #include <libdragon.h>
 #include "mem_pool.h"
 
+/**
+ * @brief Loads and allocate a sprite_t on the MemZone.
+ *
+ * @param memory_pool
+ *        MemZone to allocate the sprite_t.
+ * @param sprite_path
+ *        Path to the sprite (eg.: "/sprites/my_sprite.sprite").
+ *
+ * @return The new sprite.
+ */
 sprite_t *load_spritesheet(MemZone *memory_pool, const char *sprite_path) {
 	int fp = dfs_open(sprite_path);
 
