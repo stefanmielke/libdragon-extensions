ba7f2b908a1b5601073af85f200928864699cfbf
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-13 00:24:06 -0300

    Document tiled_cached.h

--- include/tiled_cached.h
+++ include/tiled_cached.h
@@ -5,22 +5,54 @@
 #include "rect.h"
 #include "position_int.h"
 
+/**
+ * @brief Struct the positions of a single tile type.
+ */
 typedef struct {
 	size_t count;
 	PositionInt *position;
 } TiledCachedTile;
 
+/**
+ * @brief Struct that holds a TiledCached map.
+ */
 typedef struct {
+	/// All tiles cached. The index is the tile as it is in the CSV file.
 	TiledCachedTile tiles[255];
+	/// Size of the map in tiles.
 	Size map_size;
+	/// Size of a tile.
 	Size tile_size;
+	/// Sprite used to render the tiles.
 	sprite_t *sprite;
 } TiledCached;
 
-// Init a Tiled map
-// On my tests, you can load a map with around 2_500 (50x50) tiles before an error occurs
+/**
+ * @brief Allocates, loads, and initializes the TiledCached map.
+ *
+ * @param memory_pool
+ *        MemZone to use to allocate.
+ * @param sprite
+ *        Sprite used to render.
+ * @param map_path
+ *        Path to the map file (eg.: "/maps/my_map.csv"). Has to be a CSV file.
+ * @param map_size
+ *        Size of the map in tiles.
+ * @param tile_size
+ *        Size of each tile.
+ *
+ * @return The new TiledCached.
+ */
 TiledCached *tiled_cached_init(MemZone *memory_pool, sprite_t *sprite, const char *map_path,
 							   Size map_size, Size tile_size);
 
-// Render a Tiled map
+/**
+ * @brief Render a Tiled map using hardware rendering. Use this method when there aren't that many
+ * different tiles, but they are more spread out. See 'Tiled' for other use-cases.
+ *
+ * @param tiled
+ *        TiledCached to render.
+ * @param screen_rect
+ *        Rect of the current screen. Used to cull tiles outside of the screen.
+ */
 void tiled_cached_render(TiledCached *tiled, Rect screen_rect);
