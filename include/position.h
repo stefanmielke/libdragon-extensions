8a5b1dab13beab8eca3984024dde94a70c2b4eff
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-12 23:47:38 -0300

    Document position.h

--- include/position.h
+++ include/position.h
@@ -1,10 +1,18 @@
 #pragma once
 
+/**
+ * @brief Struct that has a position.
+ */
 typedef struct {
+	/// X coordinate
 	float x;
+	/// Y coordinate
 	float y;
 } Position;
 
+/**
+ * @brief Returns a new position where X and Y are zero.
+ */
 inline Position new_position_zero() {
 	Position pos;
 	pos.x = 0;
@@ -12,6 +20,12 @@ inline Position new_position_zero() {
 	return pos;
 }
 
+/**
+ * @brief Returns a new position where X and Y are the same value.
+ *
+ * @param[in] x_and_y
+ *            Value to set X and Y with.
+ */
 inline Position new_position_same(float x_and_y) {
 	Position pos;
 	pos.x = x_and_y;
@@ -19,6 +33,14 @@ inline Position new_position_same(float x_and_y) {
 	return pos;
 }
 
+/**
+ * @brief Returns a new position where X and Y are the values sent.
+ *
+ * @param[in] x
+ *            Value to set X.
+ * @param[in] y
+ *            Value to set Y.
+ */
 inline Position new_position(float x, float y) {
 	Position pos;
 	pos.x = x;
