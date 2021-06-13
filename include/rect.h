4eae3c6b1c771a2ee405d82f86a25f770112f816
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-12 23:44:49 -0300

    Document rect.h

--- include/rect.h
+++ include/rect.h
@@ -4,11 +4,24 @@
 #include "position.h"
 #include "size.h"
 
+/**
+ * @brief Struct that has a position and a size.
+ */
 typedef struct {
+	/// Where the Rect is
 	Position pos;
+	/// Size of the Rect
 	Size size;
 } Rect;
 
+/**
+ * @brief Returns a new Rect with the position and size informed.
+ *
+ * @param[in] pos
+ *            Position of the Rect
+ * @param[in] size
+ *            Size of the Rect
+ */
 inline Rect new_rect(Position pos, Size size) {
 	Rect rect;
 	rect.pos = pos;
@@ -16,11 +29,25 @@ inline Rect new_rect(Position pos, Size size) {
 	return rect;
 }
 
+/**
+ * @brief Check if rect 'a' intersects with rect 'b'.
+ *
+ * @return If 'a' is intersecting 'b'.
+ */
 inline bool is_intersecting(Rect a, Rect b) {
 	return a.pos.x < b.pos.x + b.size.width && a.pos.x + a.size.width > b.pos.x &&
 		   a.pos.y < b.pos.y + b.size.height && a.pos.y + a.size.height > b.pos.y;
 }
 
+/**
+ * @brief Check if 'inner' is completely inside 'outer'.
+ *
+ * @param[in] inner
+ *            Rectangle that should be inside.
+ * @param[in] outer
+ *            Rectangle that should contain 'inner'.
+ * @return If 'inner' is completely inside 'outer'.
+ */
 inline bool contains(Rect inner, Rect outer) {
 	return inner.pos.x >= outer.pos.x &&
 		   inner.pos.x + inner.size.width <= outer.pos.x + outer.size.width &&
