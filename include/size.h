a4b5808ee40777994ba73c426c676d6976dd5ac6
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-13 00:06:00 -0300

    Document size.h

--- include/size.h
+++ include/size.h
@@ -1,10 +1,18 @@
 #pragma once
 
+/**
+ * @brief Struct that holds size.
+ */
 typedef struct {
+	/// Width of the struct
 	float width;
+	/// Height of the struct
 	float height;
 } Size;
 
+/**
+ * @brief Returns a new size where width and height are zero.
+ */
 inline Size new_size_zero() {
 	Size size;
 	size.width = 0;
@@ -12,6 +20,12 @@ inline Size new_size_zero() {
 	return size;
 }
 
+/**
+ * @brief Returns a new size where width and height are the same value.
+ *
+ * @param[in] width_and_height
+ *            Value to set width and height with.
+ */
 inline Size new_size_same(float width_and_height) {
 	Size size;
 	size.width = width_and_height;
@@ -19,6 +33,14 @@ inline Size new_size_same(float width_and_height) {
 	return size;
 }
 
+/**
+ * @brief Returns a new size where width and height are the values sent.
+ *
+ * @param[in] width
+ *            Value to set width.
+ * @param[in] height
+ *            Value to set height.
+ */
 inline Size new_size(float width, float height) {
 	Size size;
 	size.width = width;
