8407f1ec0e20238adf7774465d74bb247535e350
pepsiman3 <pepsiman3@github.com>
2022-12-14 08:02:03 -0700

    change format_set_render_mode to support mirroring

--- include/format_handling.h
+++ include/format_handling.h
@@ -16,37 +16,44 @@ extern "C"{
  * @return 
  *        The format of the sprite
  */
-inline tex_format_t format_set_render_mode(sprite_t *sprite){
+inline tex_format_t format_set_render_mode(sprite_t *sprite, bool mirroring){
     tex_format_t sprite_format = sprite_get_format(sprite);
     
+	if(mirroring){
+		rdpq_set_mode_standard();
+		rdpq_mode_alphacompare(ALPHACOMPARE_THRESHOLD);
+		rdpq_set_blend_color(RGBA16(0,0,0,1));
+	}
+
     switch(sprite_format){
-		case FMT_I4:
-		case FMT_I8:
+		case FMT_I4: case FMT_I8:
+			rdpq_mode_tlut(TLUT_NONE);
 			rdpq_set_mode_standard();
 			break;
-		case FMT_IA4:
+		case FMT_IA4: case FMT_IA8: case FMT_IA16:
+			rdpq_mode_tlut(TLUT_NONE);
 			rdpq_set_mode_standard();
 			rdpq_mode_alphacompare(ALPHACOMPARE_THRESHOLD);
 			rdpq_set_blend_color(RGBA16(0,0,0,1));
 			break;
 		case FMT_CI4:
-			rdpq_set_mode_copy(true);
+			if(!mirroring) rdpq_set_mode_copy(true);
 			rdpq_mode_tlut(TLUT_RGBA16);
 			rdpq_tex_load_tlut(sprite_get_palette(sprite), 0, 16);
 			break;
 		case FMT_CI8:
-			rdpq_set_mode_copy(true);
+			if(!mirroring) rdpq_set_mode_copy(true);
 			rdpq_mode_tlut(TLUT_RGBA16);
 			rdpq_tex_load_tlut(sprite_get_palette(sprite), 0, 256);
 			break;
-		case FMT_IA8:
-		case FMT_IA16:
 		case FMT_RGBA16:
 		case FMT_RGBA32:
-			rdpq_set_mode_copy(true);
+			rdpq_mode_tlut(TLUT_NONE);
+			if(!mirroring) rdpq_set_mode_copy(true);
 			break;
+		default:
+			return FMT_NONE;
 	}
-
     return sprite_format;
 }
 
