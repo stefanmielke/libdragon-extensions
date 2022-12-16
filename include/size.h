a1910959737c96cd0c97ba688c0b7bf61b2bb43c
pepsiman3 <pepsiman3@github.com>
2022-12-16 10:05:06 -0700

    Add rect_int and size_int.

--- include/size.h
+++ include/size.h
@@ -15,12 +15,28 @@ typedef struct {
 } Size;
 
 /**
+ * @brief Struct that holds an integer size.
+ */
+typedef struct {
+	/// Width of the struct
+	int width;
+	/// Height of the struct
+	int height;
+} SizeInt;
+
+/**
  * @brief Returns a new size where width and height are zero.
  */
 inline Size new_size_zero() {
-	Size size;
-	size.width = 0;
-	size.height = 0;
+	Size size = {.width = 0.f, .height = 0.f};
+	return size;
+}
+
+/**
+ * @brief Returns a new size int where width and height are zero.
+ */
+inline SizeInt new_sizeint_zero() {
+	SizeInt size = {.width = 0, .height = 0};
 	return size;
 }
 
@@ -31,9 +47,18 @@ inline Size new_size_zero() {
  *            Value to set width and height with.
  */
 inline Size new_size_same(float width_and_height) {
-	Size size;
-	size.width = width_and_height;
-	size.height = width_and_height;
+	Size size = {.width = width_and_height, .height = width_and_height};
+	return size;
+}
+
+/**
+ * @brief Returns a new size int where width and height are the same value.
+ *
+ * @param[in] width_and_height
+ *            Value to set width and height with.
+ */
+inline SizeInt new_sizeint_same(int width_and_height) {
+	SizeInt size = {.width = width_and_height, .height = width_and_height};
 	return size;
 }
 
@@ -46,9 +71,20 @@ inline Size new_size_same(float width_and_height) {
  *            Value to set height.
  */
 inline Size new_size(float width, float height) {
-	Size size;
-	size.width = width;
-	size.height = height;
+	Size size = {.width = width, .height = height};
+	return size;
+}
+
+/**
+ * @brief Returns a new size int where width and height are the values sent.
+ *
+ * @param[in] width
+ *            Value to set width.
+ * @param[in] height
+ *            Value to set height.
+ */
+inline SizeInt new_sizeint(int width, int height) {
+	SizeInt size = {.width = width, .height = height};
 	return size;
 }
 
