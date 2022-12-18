2b3260f8390f8560a4bee3d3b5320c9906e3d7ec
pepsiman3 <pepsiman3@github.com>
2022-12-18 08:18:04 -0700

    remove non-working formats for now

--- include/format_handling.h
+++ include/format_handling.h
@@ -28,39 +28,19 @@ inline tex_format_t format_set_render_mode(sprite_t *sprite, bool mirroring) {
 	}
 
 	switch (sprite_format) {
-		case FMT_I4:
-		case FMT_I8:
-			rdpq_mode_tlut(TLUT_NONE);
-			rdpq_set_mode_standard();
-			break;
-		case FMT_IA4:
-		case FMT_IA8:
-		case FMT_IA16:
-			rdpq_mode_tlut(TLUT_NONE);
-			rdpq_set_mode_standard();
-			rdpq_mode_alphacompare(ALPHACOMPARE_THRESHOLD);
-			rdpq_set_blend_color(RGBA16(0, 0, 0, 1));
-			break;
 		case FMT_CI4:
 			if (!mirroring)
 				rdpq_set_mode_copy(true);
 			rdpq_mode_tlut(TLUT_RGBA16);
 			rdpq_tex_load_tlut(sprite_get_palette(sprite), 0, 16);
 			break;
-		case FMT_CI8:
-			if (!mirroring)
-				rdpq_set_mode_copy(true);
-			rdpq_mode_tlut(TLUT_RGBA16);
-			rdpq_tex_load_tlut(sprite_get_palette(sprite), 0, 256);
-			break;
 		case FMT_RGBA16:
-		case FMT_RGBA32:
 			rdpq_mode_tlut(TLUT_NONE);
 			if (!mirroring)
 				rdpq_set_mode_copy(true);
 			break;
 		default:
-			return FMT_NONE;
+			break;
 	}
 	return sprite_format;
 }
